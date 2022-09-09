d7c6bf1b2dcf97f180f76c0d4c450726d93a077e
Siji Wang <sijiwang@usc.edu>
2022-09-08 18:43:02 -0700

    Project: CSCI526 Game prototype Rouge-like Demo 1
    Feature: Create a demo on Unity

---
+++ Rogue-like-Demo-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ���ƽ�ɫ�ƶ���������������
+/// </summary>
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // �ƶ��ٶ�
+    public float speed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ Rouge-like-Demo-2/README.md
@@ -0,0 +1,1 @@
+#### This is the directory for rogue-like-demo-2.
