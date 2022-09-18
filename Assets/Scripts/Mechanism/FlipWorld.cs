0d83bb1ff5afde9cf4a80e468177264a6e4cbc78
yizhou-Wu <wyizhou222@gmail.com>
2022-09-18 02:41:08 -0700

    feat(Project): Add flip world mechanism

---
+++ Assets/Scripts/Mechanism/FlipWorld.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipWorld : MonoBehaviour
+{
+    private GameObject[] Ceilings;
+    private GameObject[] Grounds;
+    private bool Flipped = false;
+
+    // public int Offset = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ceilings = GameObject.FindGameObjectsWithTag("Ceiling");
+        Grounds = GameObject.FindGameObjectsWithTag("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        for (int i = 0; i < Ceilings.Length; i++)
+        {
+            Vector3 pos = Ceilings[i].transform.position;
+            pos.y = -pos.y;
+            // if (!Flipped) 
+            // else pos.y += Offset;
+            Ceilings[i].transform.position = pos;
+            Ceilings[i].transform.rotation *= Quaternion.Euler(180, 0, 0);
+            // Ceilings[i].transform.position.y -= 4;
+
+        }
+        for (int i = 0; i < Grounds.Length; i++)
+        {
+            Vector3 pos = Grounds[i].transform.position;
+            pos.y = -pos.y;
+            // if(!Flipped) pos.y += Offset;
+            // else pos.y -= Offset;
+            Grounds[i].transform.position = pos;
+            Grounds[i].transform.rotation *= Quaternion.Euler(0, -180, 0);
+        }
+        Flipped = !Flipped;
+    }
+}
