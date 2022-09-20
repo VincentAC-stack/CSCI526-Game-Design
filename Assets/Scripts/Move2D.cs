47182fb893ae9fbeff4c604ad18b110cb5ed107f
VincentAC-stack <tongxu38@gmail.com>
2022-09-20 03:33:48 -0700

    fix(Scripts folder): Code change
    Fix the jump bugs and add the camera follow new feature. Delete useless
    C# scripts.

--- Assets/Scripts/Move2D.cs
+++ Assets/Scripts/Move2D.cs
@@ -6,16 +6,23 @@ using UnityEngine.SceneManagement;
 public class Move2D : MonoBehaviour
 {
     public float moveSpeed = 1.8f;
-    public bool isGrounded = false;
+    private Rigidbody2D rigidbody2d;
+    private BoxCollider2D boxCollider2d;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = transform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
@@ -29,9 +36,11 @@ public class Move2D : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space) && isTouchingGround)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,5f),ForceMode2D.Impulse);
+            // gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,5f),ForceMode2D.Impulse);
+            float jumpVelocity = 5f;
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
         }
     }
 }
