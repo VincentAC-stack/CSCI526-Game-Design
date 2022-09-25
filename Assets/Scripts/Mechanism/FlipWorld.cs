a50ded28476c1cfa2e748c6967e835b9d2995529
yizhou-Wu <wyizhou222@gmail.com>
2022-09-24 17:24:12 -0700

    fix(Assets/Scripts): Standardrized flip world tags

--- Assets/Scripts/Mechanism/FlipWorld.cs
+++ Assets/Scripts/Mechanism/FlipWorld.cs
@@ -1,23 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FlipWorld : MonoBehaviour
 {
-    private GameObject[] Ceilings;
-    private GameObject[] Grounds;
-    private GameObject[] Grass;
-	private GameObject[] Portals;
-    private bool Flipped = false;
+    private GameObject[] RotateXArr;
+    private GameObject[] RotateXAndYArr;
 
-    // public int Offset = 4;
-    // Start is called before the first frame update
     void Start()
     {
-        Ceilings = GameObject.FindGameObjectsWithTag("Ceiling");
-        Grounds = GameObject.FindGameObjectsWithTag("Ground");
-        Grass = GameObject.FindGameObjectsWithTag("grass");
- 		Portals = GameObject.FindGameObjectsWithTag("Portal");
+        RotateXArr = GameObject.FindGameObjectsWithTag("RotateX");
+        RotateXAndYArr = GameObject.FindGameObjectsWithTag("RotateXAndY");
+
     }
 
     // Update is called once per frame
@@ -28,52 +23,50 @@ public class FlipWorld : MonoBehaviour
             Flip();
         }
     }
+    void FlipY(GameObject obj)
+    {
+        Vector3 pos = obj.transform.position;
+        pos.y = -pos.y;
+        obj.transform.position = pos;
+    }
+    void RotateX(GameObject obj)
+    {
+        obj.transform.rotation *= Quaternion.Euler(180, 0, 0);
+    }
+    void RotateY(GameObject obj)
+    {
+        obj.transform.rotation *= Quaternion.Euler(0, 180, 0);
+    }
 
+    void RotateXAndY(GameObject obj)
+    {
+        obj.transform.rotation *= Quaternion.Euler(180, 180, 0);
+    }
+    //For future use 
+    // void RotateZ(GameObject obj)
+    // {
+    //     float rotationZ = obj.transform.eulerAngles.z;
+    //     obj.transform.rotation *= Quaternion.Euler(0, 180, rotationZ * 2);
+    // }
     void Flip()
     {
-        for (int i = 0; i < Ceilings.Length; i++)
+        if (RotateXArr.Length != 0)
         {
-            Vector3 pos = Ceilings[i].transform.position;
-            pos.y = -pos.y;
-            // if (!Flipped) 
-            // else pos.y += Offset;
-            Ceilings[i].transform.position = pos;
-            float rotationZ = Ceilings[i].transform.eulerAngles.z;
-            Ceilings[i].transform.rotation *= Quaternion.Euler(0, 180, rotationZ * 2);
-            // Ceilings[i].transform.position.y -= 4;
-
+            for (int i = 0; i < RotateXArr.Length; i++)
+            {
+                FlipY(RotateXArr[i]);
+                RotateX(RotateXArr[i]);
+            }
         }
-        for (int i = 0; i < Grounds.Length; i++)
+        if (RotateXAndYArr.Length  != 0)
         {
-            Vector3 pos = Grounds[i].transform.position;
-            pos.y = -pos.y;
-            // if(!Flipped) pos.y += Offset;
-            // else pos.y -= Offset;
-            Grounds[i].transform.position = pos;
-            float rotationZ = Grounds[i].transform.eulerAngles.z;
-            Grounds[i].transform.rotation *= Quaternion.Euler(0, 180, rotationZ * 2);
+            for (int i = 0; i < RotateXAndYArr.Length; i++)
+            {
+                FlipY(RotateXAndYArr[i]);
+                RotateXAndY(RotateXAndYArr[i]);
+            }
         }
-        for (int i = 0; i < Grass.Length; i++)
-        {
-            Vector3 pos = Grass[i].transform.position;
-            pos.y = -pos.y;
-            // if(!Flipped) pos.y += Offset;
-            // else pos.y -= Offset;
-            Grass[i].transform.position = pos;
-            float rotationZ = Grass[i].transform.eulerAngles.z;
-            Grass[i].transform.rotation *= Quaternion.Euler(180, 180, 0);
-        }
- 		for (int i = 0; i < Portals.Length; i++)
-        {
-            Vector3 pos = Portals[i].transform.position;
-            pos.y = -pos.y;
-            // if(!Flipped) pos.y += Offset;
-            // else pos.y -= Offset;
-            Portals[i].transform.position = pos;
-            float rotationZ = Portals[i].transform.eulerAngles.z;
-            Portals[i].transform.rotation *= Quaternion.Euler(180, 0, 0);
-        }
-        
-        Flipped = !Flipped;
     }
+    
+	
 }
