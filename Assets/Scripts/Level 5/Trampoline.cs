d87dbeb641834dcce3a27cdd88f923d61548b3d3
Siji Wang <807403713@qq.com>
2022-10-02 14:56:30 -0700

    feat(Level 5): Add the basic setting of the new mechanism of the fifth level
    New mechanisms such as fan, gravitational source and trampoline have
    been added.However, there are still problems with the up and down flip
    mechanism, which will be further corrected later.

---
+++ Assets/Scripts/Level 5/Trampoline.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float jumpForce = 22;
+
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            _animator.SetTrigger("jump");
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
