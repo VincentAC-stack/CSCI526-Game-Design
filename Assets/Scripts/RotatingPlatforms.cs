6a6d7810ade1f67bad63bbb632d0f8e280888d67
ShinonomeRyoko <Feynmaniumx@gmail.com>
2022-10-10 18:38:27 -0700

    feat(Level 1): 3 Sublevel of L1

--- Assets/Scripts/RotatingPlatforms.cs
+++ Assets/Scripts/RotatingPlatforms.cs
@@ -10,7 +10,7 @@ public class RotatingPlatforms : MonoBehaviour
     
     public static float rpm = -60f;
     private float radius = 1.5f;
-    private float centerx = 40.95f;
+    private float centerx = 7.66f;
     private float centery = 0f;
     
     
