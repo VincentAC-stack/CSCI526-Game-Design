29d3d28b8a8cc0caf6cbc8ddc118e037753eb923
yangtang <yangtang@usc.edu>
2022-10-12 17:21:53 -0700

    fix(T0-1,T0-5): T0-1 fix textguide;T0-5 fix trigger

--- Assets/Scripts/LeverTrigger.cs
+++ Assets/Scripts/LeverTrigger.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class LeverTrigger : MonoBehaviour
 {
-    private GameObject plats;
-    public static bool isAppear = false;
+    private Transform plats;
+    private Transform lever;
+    public bool isAppear = false;
+    private GameObject MovingPlats;
     
     // Start is called before the first frame update
     void Start()
     {
-        plats = GameObject.Find("Plat");
+        MovingPlats  = GameObject.Find("MovingPlats");
+        Transform[] transforms = MovingPlats.GetComponentsInChildren<Transform>();
+        foreach(Transform t in transforms)
+        {
+            // Debug.Log(t);
+            if (t.gameObject.name == "TriggeredPlatform")
+            {
+                plats = t;
+            }
+            if (t.gameObject.name == "LeverTrigger")
+            {
+                lever = t;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -21,29 +36,23 @@ public class LeverTrigger : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D col)
     {
-        // Debug.Log("Collide!");
-        // Debug.Log(col.gameObject.tag);
 
-        Vector3 lTemp = transform.localScale;
+        Vector3 lTemp = lever.localScale;
         lTemp.y *= -1;
-        transform.localScale = lTemp;
+        lever.localScale = lTemp;
      
+        
         if (isAppear)
         {
-      
             plats.GetComponent<MovingPlatformHorizontal>().enabled = false;
-            // plats.SetActive(false);
             isAppear = false;
-            
         }
         else if (!isAppear)
         {
-         
             plats.GetComponent<MovingPlatformHorizontal>().enabled = true;
-            // plats.SetActive(true);
             isAppear = true;
-            
         }
         
     }
 }
+
