c6c14f073a1660b44a91549babfda9714751c072
YuruWu <yuruw@usc.edu>
2022-11-03 22:38:21 -0700

    Fix(L3-3): modified pumpkin position and fix the related flip issue

--- Assets/Scripts/MovingPlatformsWood.cs
+++ Assets/Scripts/MovingPlatformsWood.cs
@@ -31,10 +31,14 @@ public class MovingPlatformsWood : MonoBehaviour
      void Update () {
         if (Input.GetKeyDown(KeyCode.Space) && GameController.canMove)
         {
-            childTransform.rotation *= Quaternion.Euler(180, 0, 0);
-            Vector3 pos = transform.position;
+            if(childTransform != null)
+            {
+                childTransform.rotation *= Quaternion.Euler(180, 0, 0);
+            }
+
+            Vector3 pos = childTransform.position;
             pos.y = -pos.y;
-            transform.position = pos;
+            childTransform.position = pos;
         }
         else
         {
@@ -43,9 +47,12 @@ public class MovingPlatformsWood : MonoBehaviour
      }
 
      private void move(){
-        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nexPos, speed * Time.deltaTime);
-        if (Vector3.Distance(childTransform.localPosition, nexPos) <= 0.1){
-            ChangeDestination();
+        if(childTransform != null)
+        {
+            childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nexPos, speed * Time.deltaTime);
+            if (Vector3.Distance(childTransform.localPosition, nexPos) <= 0.1){
+                ChangeDestination();
+            }
         }
      }
 
